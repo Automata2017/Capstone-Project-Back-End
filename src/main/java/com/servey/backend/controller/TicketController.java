0813227ae96bae769ba9d4a744d1544cdf57947c
CinQue Wilson <parallax707@gmail.com>
2022-02-25 17:08:31 -0500

    Edit Pom

---
+++ src/main/java/com/servey/backend/controller/TicketController.java
@@ -0,0 +1,12 @@
+package com.servey.backend.controller;
+import
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping
+public class TicketController {
+
+    @Autowired
+
+
+}
