31dfdb0f4dc9cbb7d76eca853a4fb8a6727ebce5
CinQue Wilson <parallax707@gmail.com>
2022-02-26 18:48:01 -0500

    ticket 1

--- src/main/java/com/servey/backend/model/Ticket.java
+++ src/main/java/com/servey/backend/model/Ticket.java
@@ -1,7 +1,23 @@
 package com.servey.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
 @Table(name = "Ticket")
 public class Ticket {
+    
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 
 }
