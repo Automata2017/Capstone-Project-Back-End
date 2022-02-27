de54f768e93bd62866cfb784fa2e15525920ab2c
CinQue Wilson <parallax707@gmail.com>
2022-02-26 19:10:05 -0500

    ticket repository

--- src/main/java/com/servey/backend/model/Ticket.java
+++ src/main/java/com/servey/backend/model/Ticket.java
@@ -1,23 +1,98 @@
 package com.servey.backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Ticket")
 public class Ticket {
-    
+
     @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String fullName;
+
+    @Column
+    private Long emailAddress;
+
+    @Column
+    private Integer weekNumber;
+
+    @Column
+    private Long lessonName;
+
+    @Column
+    private String studentComment;
+
+    @Column
+    private Integer studentRating;
+
+    @Column
+    private Boolean studentRecommendation;
+
     public Long getId() {
         return id;
     }
-
-    private Long id;
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Long getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(Long emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Integer getWeekNumber() {
+        return weekNumber;
+    }
+
+    public void setWeekNumber(Integer weekNumber) {
+        this.weekNumber = weekNumber;
+    }
+
+    public Long getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(Long lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public String getStudentComment() {
+        return studentComment;
+    }
+
+    public void setStudentComment(String studentComment) {
+        this.studentComment = studentComment;
+    }
+
+    public Integer getStudentRating() {
+        return studentRating;
+    }
+
+    public void setStudentRating(Integer studentRating) {
+        this.studentRating = studentRating;
+    }
+
+    public Boolean getStudentRecommendation() {
+        return studentRecommendation;
+    }
+
+    public void setStudentRecommendation(Boolean studentRecommendation) {
+        this.studentRecommendation = studentRecommendation;
+    }
 }
--- src/main/java/com/servey/backend/repository/TicketRepository.java
+++ src/main/java/com/servey/backend/repository/TicketRepository.java
@@ -1,4 +1,16 @@
 package com.servey.backend.repository;
 
-public interface TicketRepository {
+import com.servey.backend.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    Ticket findByFullNameAndEmailAddress(String fullName, Long emailAddress);
+    Ticket findById (long id);
+
+    @Override
+    List<Ticket> findAllById(Iterable<Long> longs);
 }
