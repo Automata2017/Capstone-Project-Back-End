07e3026c089ee71b6edb601d6dd72eb0da765919
CinQue Wilson <parallax707@gmail.com>
2022-02-26 19:23:57 -0500

    ticket controller 2

--- src/main/java/com/servey/backend/controller/TicketController.java
+++ src/main/java/com/servey/backend/controller/TicketController.java
@@ -1,5 +1,6 @@
 package com.servey.backend.controller;
 
+import com.servey.backend.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api")
 public class TicketController {
-    
+
+    private TicketService ticketService;
+
 }
