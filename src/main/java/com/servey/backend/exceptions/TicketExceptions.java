8a2629e1a1367db1e4eca3a3c69f68699a4cd1b7
CinQue Wilson <parallax707@gmail.com>
2022-03-01 20:39:17 -0500

    comments

--- src/main/java/com/servey/backend/exceptions/TicketExceptions.java
+++ src/main/java/com/servey/backend/exceptions/TicketExceptions.java
@@ -3,7 +3,7 @@ package com.servey.backend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND) //A message for when tickets or http link are not found
 public class TicketExceptions extends RuntimeException{
     public TicketExceptions(String message){
         super(message);
